ff3105718346074e447be076e4660f7d15b64233
MicheleProcacciante <81179178+MicheleProcacciante@users.noreply.github.com>
2021-04-13 11:34:16 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -7,6 +7,22 @@ var cpuPick;
 
 var allOptions = ["sasso", "carta","forbice"];
 
+var allOptions = [
+  {
+    name: "sasso" ,
+    image: "assets/sasso.png"
+  },
+  {
+    name: "carta" ,
+    image: "assets/carta.png"
+  },
+  {
+    name: "forbice" ,
+    image: "assets/forbice.png"
+  },
+  
+]
+
 var possibleUserPick = document.getElementsByClassName("user-choice");
 
 for(var i = 0; i<possibleUserPick.length; i++){
@@ -17,8 +33,11 @@ function onUserPick(){
   userPick=this.dataset.name;
  console.log("la tua scelta è :",userPick);
  generateCpuPick();
- console.log("la  scelta del computer è :",cpuPick);
- checkWhoWon();
+ console.log("la  scelta del computer è :",cpuPick.name);
+ console.log(checkWhoWon());
+ var cpuPickImage = "<img src'"+ cpuPick.image +"'/>";
+ var cpuPickText = "<h3>" + cpuPick.name + "</h3>";
+ document.getElementById("computer-choice") .innerHTML = cpuPickImage + cpuPickText;
 }
 
 
@@ -31,32 +50,34 @@ function generateCpuPick(){
 generateCpuPick();
 
 function checkWhoWon(){
- if(userPick==cpuPick){
- console.log("hai pareggiato"); 
-}
-                else{
-                    if(userPick=="sasso"){
-                        if(cpuPick=="forbice"){
-                                console.log("hai vinto");
-                        } else{
-                            console.log("hai perso");
-                        }
+ if(userPick==cpuPick.name){
+   return "hai pareggiato";
+  }
+  else{
+    if(userPick=="sasso"){
+      if(cpuPick.name=="forbice"){
+      return "hai vinto";
+      } else{
+        return "hai perso";
+      }
 
-                    } else if(userPick =="carta"){
-                        if(cpuPick =="sasso"){
-                            console.log("hai vinto");
-                        } else{
-                            console.log("hai perso");
-                        }
-                    } else if(userPick=="forbice"){
-                        if(cpuPick=="carta"){
-                            console.log("hai vinto");
-                        } else{
-                            console.log("hai perso");
-                        }                   
-                    }
-                }
-            }
-document.getElementById("result").innerHTML = cpuPick;
+      }  if(userPick =="carta"){
+      if(cpuPick.name =="sasso"){
+        return "hai vinto";
+      } else{
+        return "hai perso";
+      }
+      }  if(userPick=="forbice"){
+      if(cpuPick.name=="carta"){
+        return "hai vinto";
+      } else{
+        return "hai perso";
+      }                   
+    }
+  }
+ 
+}
+
+
 
 			
